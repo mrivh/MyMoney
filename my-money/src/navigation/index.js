b706340408183cc4c12617cf1d8c15b396effc6c
mrivh <m.alicia.riv@gmail.com>
2022-08-14 15:18:39 -0500

    Add: Tab Bar Icons

--- my-money/src/navigation/index.js
+++ my-money/src/navigation/index.js
@@ -3,6 +3,7 @@ import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Icon from "react-native-vector-icons/Ionicons";
 import LoginScreen from "../screens/loginScreen";
 import RegisterScreen from "../screens/registerScreen";
 import HomeScreen from "../screens/home";
@@ -15,7 +16,7 @@ import ListIncomeScreen from "../screens/listIncomeScreen";
 import RegisterExpenseScreen from "../screens/registerExpenseScreen";
 import ListExpenseScreen from "../screens/listExpenseScreen";
 import RegisterSavingScreen from "../screens/registerSavingScreen";
-import ListSavingScreen from "../screens/listSavingScreen"
+import ListSavingScreen from "../screens/listSavingScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -27,13 +28,28 @@ function Logo() {
 function RegisterAccountsTab() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Registro Cuentas") {
+            iconName = focused
+              ? "md-create"
+              : "md-create-outline";
+          } else if (route.name === "Lista Cuentas") {
+            iconName = focused ? "md-list-sharp" : "md-list-outline";
+          }
+          return <Icon name={iconName} size={35} color={color} />;
+        },
+        tabBarActiveTintColor: "#00296A",
+        tabBarInactiveTintColor: "gray",
         headerStyle: {
           backgroundColor: "#00296A",
         },
         headerTitleAlign: "center",
         headerTintColor: "#FFFFFF",
-      }}
+        tabBarShowLabel: false,
+      })}
     >
       <Tab.Screen name="Registro Cuentas" component={RegisterAccountsScreen} />
       <Tab.Screen name="Lista Cuentas" component={ListAccountsScreen} />
@@ -44,13 +60,28 @@ function RegisterAccountsTab() {
 function RegisterTagsTab() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#00296A",
-        },
-        headerTitleAlign: "center",
-        headerTintColor: "#FFFFFF",
-      }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Registro Etiquetas") {
+          iconName = focused
+            ? "md-create"
+            : "md-create-outline";
+        } else if (route.name === "Lista Etiquetas") {
+          iconName = focused ? "md-list-sharp" : "md-list-outline";
+        }
+        return <Icon name={iconName} size={35} color={color} />;
+      },
+      tabBarActiveTintColor: "#00296A",
+      tabBarInactiveTintColor: "gray",
+      headerStyle: {
+        backgroundColor: "#00296A",
+      },
+      headerTitleAlign: "center",
+      headerTintColor: "#FFFFFF",
+      tabBarShowLabel: false,
+    })}
     >
       <Tab.Screen name="Registro Etiquetas" component={RegisterTagsScreen} />
       <Tab.Screen name="Lista Etiquetas" component={ListTagsScreen} />
@@ -61,13 +92,28 @@ function RegisterTagsTab() {
 function RegisterIncomeTab() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#00296A",
-        },
-        headerTitleAlign: "center",
-        headerTintColor: "#FFFFFF",
-      }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Registro Ingresos") {
+          iconName = focused
+            ? "md-create"
+            : "md-create-outline";
+        } else if (route.name === "Lista Ingresos") {
+          iconName = focused ? "md-list-sharp" : "md-list-outline";
+        }
+        return <Icon name={iconName} size={35} color={color} />;
+      },
+      tabBarActiveTintColor: "#00296A",
+      tabBarInactiveTintColor: "gray",
+      headerStyle: {
+        backgroundColor: "#00296A",
+      },
+      headerTitleAlign: "center",
+      headerTintColor: "#FFFFFF",
+      tabBarShowLabel: false,
+    })}
     >
       <Tab.Screen name="Registro Ingresos" component={RegisterIncomeScreen} />
       <Tab.Screen name="Lista Ingresos" component={ListIncomeScreen} />
@@ -78,13 +124,28 @@ function RegisterIncomeTab() {
 function RegisterExpenseTab() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#00296A",
-        },
-        headerTitleAlign: "center",
-        headerTintColor: "#FFFFFF",
-      }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Registro Gastos") {
+          iconName = focused
+            ? "md-create"
+            : "md-create-outline";
+        } else if (route.name === "Lista Gastos") {
+          iconName = focused ? "md-list-sharp" : "md-list-outline";
+        }
+        return <Icon name={iconName} size={35} color={color} />;
+      },
+      tabBarActiveTintColor: "#00296A",
+      tabBarInactiveTintColor: "gray",
+      headerStyle: {
+        backgroundColor: "#00296A",
+      },
+      headerTitleAlign: "center",
+      headerTintColor: "#FFFFFF",
+      tabBarShowLabel: false,
+    })}
     >
       <Tab.Screen name="Registro Gastos" component={RegisterExpenseScreen} />
       <Tab.Screen name="Lista Gastos" component={ListExpenseScreen} />
@@ -95,13 +156,28 @@ function RegisterExpenseTab() {
 function RegisterSavingTab() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#00296A",
-        },
-        headerTitleAlign: "center",
-        headerTintColor: "#FFFFFF",
-      }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Registro Ahorros") {
+          iconName = focused
+            ? "md-create"
+            : "md-create-outline";
+        } else if (route.name === "Lista Ahorros") {
+          iconName = focused ? "md-list-sharp" : "md-list-outline";
+        }
+        return <Icon name={iconName} size={35} color={color} />;
+      },
+      tabBarActiveTintColor: "#00296A",
+      tabBarInactiveTintColor: "gray",
+      headerStyle: {
+        backgroundColor: "#00296A",
+      },
+      headerTitleAlign: "center",
+      headerTintColor: "#FFFFFF",
+      tabBarShowLabel: false,
+    })}
     >
       <Tab.Screen name="Registro Ahorros" component={RegisterSavingScreen} />
       <Tab.Screen name="Lista Ahorros" component={ListSavingScreen} />
